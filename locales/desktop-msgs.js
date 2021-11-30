7c980a215ae919f39a0377daaa14e9d37cd2bfc3
txlfreedom <1093552791@qq.com>
2021-11-30 21:13:11 +0800

    1

---
+++ locales/desktop-msgs.js
@@ -0,0 +1,35 @@
+// GENERATED FILE:
+export default {
+    'en': {
+        'index.cameraPermissionDeniedMessage': 'Permission to use the camera has been denied. Scratch will not be able to take a photo or use video sensing blocks.',
+        'index.cameraPermissionDeniedTitle': 'Camera Permission Denied',
+        'index.darwinPermissionDeniedInstructions': 'To change Scratch permissions, please check "Security & Privacy" in System Preferences.',
+        'index.failedLoadProject': 'Could not load project from file:',
+        'index.leave': 'Leave',
+        'index.microphonePermissionDeniedMessage': 'Permission to use the microphone has been denied. Scratch will not be able to record sounds or detect loudness.',
+        'index.microphonePermissionDeniedTitle': 'Microphone Permission Denied',
+        'index.permissionDeniedInstructions': 'To change Scratch permissions, please check your system settings and restart Scratch.',
+        'index.permissionDeniedMessage': 'A permission has been denied.',
+        'index.permissionDeniedTitle': 'Permission Denied',
+        'index.questionLeave': 'Leave Scratch?',
+        'index.questionLeaveDetail': 'Any unsaved changes will be lost.',
+        'index.saveFailed': 'Save failed:',
+        'index.stay': 'Stay'
+    },
+    'zh-cn': {
+        'index.cameraPermissionDeniedMessage': '使用摄像头的权限已被拒绝。Scratch 将无法拍照或使用视频侦测块。',
+        'index.cameraPermissionDeniedTitle': '摄像头权限被拒绝',
+        'index.darwinPermissionDeniedInstructions': '要更改 Scratch 的权限，请检查 “系统首选项” 中的 “安全和隐私” 设定。',
+        'index.failedLoadProject': '无法从文件加载项目：',
+        'index.leave': '离开',
+        'index.microphonePermissionDeniedMessage': '使用麦克风的权限已被拒绝。 Scratch 将无法录制声音或检测响度。',
+        'index.microphonePermissionDeniedTitle': '麦克风许可被拒绝',
+        'index.permissionDeniedInstructions': '要更改 Scratch 的权限，请检查系统设置并重新启动 Scratch',
+        'index.permissionDeniedMessage': '权限已被拒绝。',
+        'index.permissionDeniedTitle': '没有权限',
+        'index.questionLeave': '离开 Scratch？',
+        'index.questionLeaveDetail': '任何未保存的更改将丢失。',
+        'index.saveFailed': '保存失败：',
+        'index.stay': '留下'
+    }
+};
